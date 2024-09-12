a86cd6adff6cce1a7a14a63428a54c10bad9d131
Victor A. Animasahun <ashonibarevik@gmail.com>
2024-09-12 23:05:23 +0100

    pop

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    allf = dir()
+    for m in range(0, len(allf)):
+        if allf[m][:2] != "__":
+            print("{s}".format(allf[m]))
