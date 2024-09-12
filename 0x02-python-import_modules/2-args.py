4825d2667cb08c179cb8ac89092d086cb099b63d
Victor A. Animasahun <ashonibarevik@gmail.com>
2024-09-12 21:33:28 +0100

    pop

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    
+
     # Retrieve the number of arguments (excluding the script name)
     num_args = len(sys.argv) - 1
-    
+
     # Determine the correct wording for "argument" or "arguments"
     if num_args == 0:
         print("0 arguments.")
     else:
         print("{} argument{}:".format(num_args, "" if num_args == 1 else "s"))
-        
+
         # Print each argument with its position
         for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
