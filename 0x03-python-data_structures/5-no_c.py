a744b89e5f3a6bfa50d8fc8de82712aae8cebaa7
Victor A. Animasahun <ashonibarevik@gmail.com>
2024-09-17 09:15:55 +0100

    first batch

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-        return my_string.replace('c', '').replace('C', '')
+    # Use string translation to remove 'c' and 'C'
+    return my_string.translate(str.maketrans('', '', 'cC'))
