d9897eb748863c979fd7dff9e3dc8ebec9a5882c
Victor A. Animasahun <ashonibarevik@gmail.com>
2024-09-17 23:33:36 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -21,6 +21,7 @@ listint_t *reverse_list(listint_t *head)
 		prev = current;
 		current = next;
 	}
+
 	return (prev);
 }
 
@@ -32,31 +33,37 @@ listint_t *reverse_list(listint_t *head)
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow = *head, *fast = *head;
-    listint_t *first_half_end = NULL, *second_half_start = NULL;
-    listint_t *first_half_start, *second_half_copy;
+	listint_t *slow = *head, *fast = *head;
+	listint_t *first_half_end = NULL, *second_half_start = NULL;
+	listint_t *first_half_start, *second_half_copy;
 
-    if (*head == NULL || (*head)->next == NULL)
-        return (1);
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        first_half_end = slow;
-        slow = slow->next;
-        fast = fast->next->next;
-    }
+	while (fast != NULL && fast->next != NULL)
+	{
+		first_half_end = slow;
+		slow = slow->next;
+		fast = fast->next->next;
+	}
 
-    second_half_start = reverse_list(slow);
+	second_half_start = reverse_list(slow);
 
-    first_half_start = *head;
-    second_half_copy = second_half_start;
+	first_half_start = *head;
+	second_half_copy = second_half_start;
 
-    while (second_half_start != NULL)
-    {
-        if (first_half_start->n != second_half_start->n)
-        {
-            reverse_list(second_half_copy);
-            return (0);
-        }
-        first_half_start = first_half_start->next;
-        second
+	while (second_half_start != NULL)
+	{
+		if (first_half_start->n != second_half_start->n)
+		{
+			reverse_list(second_half_copy);
+			return (0);
+		}
+		first_half_start = first_half_start->next;
+		second_half_start = second_half_start->next;
+	}
+
+	reverse_list(second_half_copy);
+
+	return (1);
+}
