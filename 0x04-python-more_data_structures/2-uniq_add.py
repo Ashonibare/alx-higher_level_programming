a1cb5007f1a2fee33f703d1119683c9eaf3db92d
Ashonibare <Ashonibarevik@gmail.com>
2024-09-18 13:57:11 -0700

    replace

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+def uniq_add(my_list=[]):
+    unique_integers = set()  # Set to track unique integers
+    total_sum = 0  # Initialize sum to 0
+
+    # Loop through each integer in the list
+    for number in my_list:
+        #  check if int is not in set
+        if number not in unique_integers:
+            unique_integers.add(number)  # Add the integer to the set
+            total_sum += number  # Add the integer to the total sum
+
+    return total_sum  # Return the sum of unique integers
