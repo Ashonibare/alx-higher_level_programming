fde169a50541894aee6fa0d2fec6bd1d7888a58a
Ashonibare <Ashonibarevik@gmail.com>
2024-09-18 12:47:03 -0700

    new

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def square_matrixsimple(matrix=[]):
+def square_matrix_simple(matrix=[]):
     squared_matrix = []  # empty list to store the sqaured ints
 
     for row in matrix:  # loop through each row
-        squred_row = []  # to store the squared values in rows
+        squared_row = []  # to store the squared values in rows
 
         for element in row:  # loops through each int in each row
             squared_row.append(element ** 2)  # square each int, add to list
