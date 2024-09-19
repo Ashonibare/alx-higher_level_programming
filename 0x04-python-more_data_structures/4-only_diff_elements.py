668f3581860e95564c787e95d5cee79a4fdee0ba
Victor A. Animasahun <ashonibarevik@gmail.com>
2024-09-19 01:04:13 +0100

    second batch

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # Find elements that are in either set_1 or set_2, but not in both
+    # Find elements in either set_1 or set_2, but not in both
     return set_1 ^ set_2
